abe1c901a9020722dfbeda5db8244769f589bb12
coderanishka <62501236+coderanishka@users.noreply.github.com>
2022-04-06 17:14:24 +0530

    Add files via upload

---
+++ screens/Main.js
@@ -0,0 +1,110 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Component } from "react";
+import { StyleSheet, Text, View, SafeAreaView, Platform, Image, ScrollView, TouchableOpacity } from "react-native";
+import {Camera} from 'expo-camera'
+import * as FaceDetector from 'expo-face-detector'
+
+export default class Main extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            hasCameraPermission: null,
+            faces: []
+        }
+        this.onFaceDetected = this.onFaceDetected.bind(this)
+    }
+ 
+    async componentDidMount(){
+        const{status} = await Camera.requesPermissionAsync()
+        this.setState({hasCameraPermission: status === 'granted'})
+    }
+
+    onFacesDetected({faces}){
+        this.setState({ faces:faces})
+    }
+
+    render(){
+        var { hasCameraPermission } = this.state;
+        if (hasCameraPermission === null) {
+            return <View />;
+        }
+        if (hasCameraPermission === false) {
+        return (
+            <View style={styles.container}>
+            <Text>No access to camera</Text>
+            </View>
+        )
+
+        return(
+            <View style = {style.container}>
+                <SafeAreaView style = {styles.droidSafeArea}/>
+                <View style = {styles.upperContainer}>
+                    <Image 
+                    source = {require('../assets/appIcon.png')}
+                    style = {styles.appIcon} 
+                    />
+                    <Text style = {styles.appName}>Look me!</Text>
+                </View>
+                <View style={styles.middleContainer}>
+                    <Camera style = {{flex:1}}
+                    type = {Camera.Constants.Type.front}
+                    faceDetectorSettings = {{
+                        mode: FaceDetector.Constants.Mode.fast,
+                        detectLandmarks: FaceDetector.Constants.Landmarks.all,
+                        runClassifications: FaceDetector.Constants.Classifications.all
+                    }}
+                    onFaceDetected = {this.onFaceDetected}
+                    onFacesdetectionError = {this.onFacesdetectionError}
+                    />
+                </View> 
+                <View style={styles.lowerContainer}>
+                    <View style={styles.lowerTopContainer}></View>
+                    <View style={styles.lowerBottomContainer}></View>
+                </View>
+            </View>
+        )
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#E7F2F8"
+    },
+    droidSafeArea: {
+      marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    upperContainer: {
+      flex: 0.13,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#E7F2F8",
+      flexDirection: "row"
+    },
+    appIcon: {
+      width: 50,
+      height: 50,
+      borderRadius: 25
+    },
+    appName: {
+      fontSize: 25
+    },
+    middleContainer: { flex: 0.67 },
+    lowerContainer: {
+      flex: 0.2,
+      backgroundColor: "#E7F2F8"
+    },
+    lowerTopContainer: {
+      flex: 0.3,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    lowerBottomContainer: {
+      flex: 0.7,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#EFE7BC"
+    }
+  });
